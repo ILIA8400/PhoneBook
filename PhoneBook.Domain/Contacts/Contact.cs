0dd48ef2a1e2d6e99192cb0fd2ee83dab10458c8
ilia <iliaasgharhorri@gmail.com>
2024-11-01 19:29:13 +0330

    Phone number changed to type of long

--- PhoneBook.Domain/Contacts/Contact.cs
+++ PhoneBook.Domain/Contacts/Contact.cs
@@ -13,7 +13,7 @@ namespace PhoneBook.Domain.Contacts
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string PhoneNumber{ get; set; }
+        public long PhoneNumber{ get; set; }
         public string? Email { get; set; }
         public string? Address { get; set; }
         public ICollection<ContactGroup>? Groups { get; set; }
