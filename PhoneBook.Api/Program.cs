2108be0fceacbc37d1e21d01cbd561e137e65b5b
ilia <iliaasgharhorri@gmail.com>
2024-11-03 21:18:46 +0330

    All settings of the layers were transferred to Program in the api layer

--- PhoneBook.Api/Program.cs
+++ PhoneBook.Api/Program.cs
@@ -1,6 +1,14 @@
-using PhoneBook.Application;
-using PhoneBook.Infra.Identity;
-using PhoneBook.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using PhoneBook.Application.Contracts.BaseRepositories;
+using PhoneBook.Application.Contracts.CallHistories;
+using PhoneBook.Application.Contracts.Contacts;
+using PhoneBook.Application.Contracts.Groups;
+using PhoneBook.Application.DTOs.Contacts;
+using PhoneBook.Infra;
+using PhoneBook.Infra.Repositories;
+using PhoneBook.Identity;
+using PhoneBook.Identity.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,10 +23,23 @@ builder.Services.AddSwaggerGen();
 builder.Configuration.AddUserSecrets("be12b960-bb41-4af9-98d2-8f11fdc978e4");
 
 // Services
-builder.Services.ConfigureApplicationServices();
-builder.Services.ConfigureIdentityServices(builder.Configuration);
-builder.Services.ConfigurePersistenceServices(builder.Configuration);
+//builder.Services.ConfigureApplicationServices();
+//builder.Services.ConfigureIdentityServices(builder.Configuration);
+//builder.Services.ConfigurePersistenceServices(builder.Configuration);
+builder.Services.AddMediatR(c => c.RegisterServicesFromAssemblies(typeof(CreateContactDto).Assembly));
 
+//Add DbContexts
+builder.Services.AddDbContext<PhoneBookIdentityDbContext>(c=>c.UseSqlServer(builder.Configuration["ConnectionStrings:cnn1"]));
+builder.Services.AddDbContext<PhoneBookDbContext>(c => c.UseSqlServer(builder.Configuration["ConnectionStrings:cnn2"]));
+
+// Add Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<PhoneBookIdentityDbContext>();
+
+// Repositories
+builder.Services.AddScoped<ICallHistoryRepository, CallHistoryRepository>();
+builder.Services.AddScoped<IContactRepository, ContactRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IGroupRepository, GroupRepository>();
 
 var app = builder.Build();
 
