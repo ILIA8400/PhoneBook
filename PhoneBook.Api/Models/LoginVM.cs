ebd6b977cd1e65c88e741b7c1ecd406269ba6d20
ilia <iliaasgharhorri@gmail.com>
2024-11-03 21:12:38 +0330

    View Models User Controller was Creted

---
+++ PhoneBook.Api/Models/LoginVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBook.Api.Models
+{
+    public class LoginVM
+    {
+        [EmailAddress]
+        public required string Email { get; set; }
+        [StringLength(255, MinimumLength = 8)]
+        public required string Password { get; set; }
+    }
+}
---
+++ PhoneBook.Api/Models/RegisterVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBook.Api.Models
+{
+    public class RegisterVM
+    {
+        [EmailAddress]
+        public required string Email { get; set; }
+        [StringLength(255,MinimumLength =8)]
+        public required string Password { get; set; }
+        [StringLength(255, MinimumLength = 8)]
+        [Compare("Password")]
+        public required string RepeatPassword { get; set; }
+    }
+}
