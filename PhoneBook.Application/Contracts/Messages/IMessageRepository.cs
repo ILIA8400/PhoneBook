e37a0165f1c710c9669d0c0c2e239544a479ec2a
ilia <iliaasgharhorri@gmail.com>
2024-11-08 11:06:27 +0330

    Add Message Contract Repository

---
+++ PhoneBook.Application/Contracts/Messages/IMessageRepository.cs
@@ -0,0 +1,14 @@
+﻿using PhoneBook.Application.Contracts.BaseRepositories;
+using PhoneBook.Domain.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Application.Contracts.Messages
+{
+    public interface IMessageRepository : IGenericRepository<Message>
+    {
+    }
+}
