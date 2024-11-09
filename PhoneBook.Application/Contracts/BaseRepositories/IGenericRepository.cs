439d17152a286a1203bd8e2728ce2504852c6f4b
ilia <iliaasgharhorri@gmail.com>
2024-11-09 14:34:49 +0330

    Add UserId in IRepositories

--- PhoneBook.Application/Contracts/BaseRepositories/IGenericRepository.cs
+++ PhoneBook.Application/Contracts/BaseRepositories/IGenericRepository.cs
@@ -8,11 +8,11 @@ namespace PhoneBook.Application.Contracts.BaseRepositories
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<T> Get(int id);
-        Task<IReadOnlyList<T>> GetAll();
-        Task<bool> Exist(int id);
-        Task<T> Add(T entity);
-        Task Update(T entity);
-        Task Delete(T entity);
+        Task<T> Get(int id,int userId);
+        Task<IReadOnlyList<T>> GetAll(int userId);
+        Task<bool> Exist(int id, int userId);
+        Task<T> Add(T entity, int userId);
+        Task Update(T entity, int userId);
+        Task Delete(T entity, int userId);
     }
 }
