9a5dd1577257c45aa2225ed669f84d16d4914f0f
ilia <iliaasgharhorri@gmail.com>
2024-11-14 15:19:50 +0330

    Add ContactGroup Dto

---
+++ PhoneBook.Application/DTOs/ContactGroup/Validators/AddContactGroupValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Application.DTOs.ContactGroup.Validators
+{
+    public class AddContactGroupValidator : AbstractValidator<ContactGroupDto>
+    {
+        public AddContactGroupValidator()
+        {
+            RuleFor(c => c.ContatcsIds).NotNull().WithMessage("این فیلد نباید خالی باشه");
+        }
+    }
+}
