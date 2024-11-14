94a66bd6bad1fb05a624f32a90094b2853d0ca2b
ilia <iliaasgharhorri@gmail.com>
2024-11-15 00:35:55 +0330

    add save changes to IGenericRepo

--- PhoneBook.Application/Contracts/BaseRepositories/IGenericRepository.cs
+++ PhoneBook.Application/Contracts/BaseRepositories/IGenericRepository.cs
@@ -14,5 +14,6 @@ namespace PhoneBook.Application.Contracts.BaseRepositories
         Task<T> Add(T entity);
         Task Update(T entity);
         Task Delete(T entity);
+        Task SaveChanges();
     }
 }
